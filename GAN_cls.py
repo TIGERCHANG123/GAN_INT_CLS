da699940bfc706e02321823112ea640d3ee5a296
TIGERC123 <2462924814@qq.com>
2020-03-11 21:28:35 +0800

    GAN-INT-CLS

--- GAN_cls.py
+++ GAN_cls.py
@@ -36,8 +36,8 @@ class discriminator(tf.keras.Model):
     self.output_layer = discriminator_Output(with_activation=False)
 
   def call(self, text, x):
-    print('text shape: {}'.format(text.shape))
-    print('x shape: {}'.format(x.shape))
+    # print('text shape: {}'.format(text.shape))
+    # print('x shape: {}'.format(x.shape))
     code = self.encoder(text)
     code = tf.expand_dims(code, axis=1)
     code = tf.expand_dims(code, axis=2)
